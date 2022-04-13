fc3189bf8a4d3b118bfe5c5018d687e2da89e305
Ljubo Nikolic <ljubonikolic@gmail.com>
2022-04-13 21:13:38 +0200

    comments

--- .github/workflows/scripts/workflow.js
+++ .github/workflows/scripts/workflow.js
@@ -1,14 +1,17 @@
-module.exports = async ({ core, context }, token, owner, repo, ref, workflow_name) => {
+module.exports = async ({ core, context }, token) => {
   const { Octokit } = require("@octokit/core")
   const octokit = new Octokit({ auth: token })
 
-  // TODO: change this to G-Research/charts
+  const owner = `ljubon` // TODO: Change to G-Research
+  const repo = `charts` // TODO: Change to charts
+  const ref = `INTERNAL-master` // TODO: Change to chart's default branch (master)
+  const workflow_name = `Push` // NOTE: Workflow name in chart repo must be named `Push` (push.yaml in this code)
+
   const { data: repo_workflows } = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
     owner: owner,
     repo: repo,
   })
 
-  // TODO: workflow name must be set to `Push`
   var push_workflow_id = ''
   for (const workflow of repo_workflows.workflows) {
     if (workflow.name == workflow_name) {
@@ -16,7 +19,7 @@ module.exports = async ({ core, context }, token, owner, repo, ref, workflow_nam
     }
   }
   if (push_workflow_id == '') {
-    core.setFailed(`Unable to find workflow called "Push" on ljubon/charts `)
+    core.setFailed(`Unable to find workflow called "Push" on ${context.payload.repository.owner.login}/${context.payload.repository.name}`)
   }
 
   try {
@@ -31,8 +34,15 @@ module.exports = async ({ core, context }, token, owner, repo, ref, workflow_nam
         ref: context.payload.ref
       },
     })
+
+    // API: https://docs.github.com/en/rest/reference/apps#revoke-an-installation-access-token
+    console.log(`Revoking the token...`)
+    await octokit.request('DELETE /installation/token', {})
+
   } catch (error) {
     return core.setFailed(`Unable to trigger workflow dispatch on ${owner}/${repo}.\n${error}`)
   }
+
+
 
 }
