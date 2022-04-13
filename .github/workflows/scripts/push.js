c840ebe3ac116a228050c9b2d8b3a546a513ad2a
Ljubo Nikolic <ljubonikolic@gmail.com>
2022-04-13 21:13:38 +0200

    comments

--- .github/workflows/scripts/push.js
+++ .github/workflows/scripts/push.js
@@ -1,4 +1,6 @@
-module.exports = async ({ core, exec }, token) => {
+module.exports = async ({ core, exec, context }, token) => {
+  const { Octokit } = require("@octokit/core")
+  const octokit = new Octokit({ auth: token })
   const checkoutPageDir = "gh-pages"
   const helm = {
     owner: process.env.OWNER,
@@ -7,15 +9,15 @@ module.exports = async ({ core, exec }, token) => {
     charts: JSON.parse(process.env.CHARTS)
   }
 
-  const headerToken_b64 = btoa(`x-access-token:${token}`) // TODO: chek if we can use buf.toString('base64') (docc)
-  core.debug(headerToken_b64)
+  const headerToken_b64 = Buffer.from(`x-access-token:${token}`).toString('base64')
+  core.setSecret(headerToken_b64)
 
   try {
     // Verify status and set git config
     await exec.exec('git', ['remote', '-v'], { cwd: checkoutPageDir })
     await exec.exec('git', ['status'], { cwd: checkoutPageDir })
     await exec.exec('git', ['config', '--local', '--unset-all', 'http.https://github.com/.extraheader'], { cwd: checkoutPageDir })
-    await exec.exec('git', ['config', '--local', 'http.https://github.com/.extraheader', `::add-mask::AUTHORIZATION: basic ${headerToken_b64}`], { cwd: checkoutPageDir })
+    await exec.exec('git', ['config', '--local', 'http.https://github.com/.extraheader', `AUTHORIZATION: basic ${headerToken_b64}`], { cwd: checkoutPageDir })
     await exec.exec('git', ['config', '--local', 'user.name', 'G-Research charts'], { cwd: checkoutPageDir })
     await exec.exec('git', ['config', '--local', 'user.email', 'charts@gr-oss.io'], { cwd: checkoutPageDir })
 
@@ -28,13 +30,14 @@ module.exports = async ({ core, exec }, token) => {
 
     // Commit and push files
     await exec.exec('git', ['status'], { cwd: checkoutPageDir })
-    await exec.exec('git', ['commit', '-m', `Publish helm chart to ljubon/charts`, '--verbose'], { cwd: checkoutPageDir }) // TODO: pull out ljubon/chart from github context owner/repo
+    await exec.exec('git', ['commit', '-m', `Publish helm chart to ${context.payload.repository.owner.login}/${context.payload.repository.name}`, '--verbose'], { cwd: checkoutPageDir }) 
     await exec.exec('git', ['push', 'origin', checkoutPageDir, '--verbose'], { cwd: checkoutPageDir })
+
+    // API: https://docs.github.com/en/rest/reference/apps#revoke-an-installation-access-token
+    console.log(`Revoking the token...`)
+    await octokit.request('DELETE /installation/token', {})
+
   } catch (error) {
     return core.setFailed(`Unable to push ${checkoutPageDir}/${helm.charts.destination} to ljubon/charts@${checkoutPageDir}\nError: ${error}`)
   }
-
-  // TODO: uninstall token https://docs.github.com/en/rest/reference/apps#revoke-an-installation-access-token
-  // suspend generated token 
-
 }
